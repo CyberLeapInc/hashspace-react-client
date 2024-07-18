26e39f4608cea6295af9127959a3b0ed316b161a
anonymous-wan <brook.wang@cyberleap.us>
2024-07-18 19:12:47 +0800

    fix: export error

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,8 @@
-let cloudFlareSiteKey ='0x4AAAAAAAVuhgDN4FXyZAFb';
-if (process.env.CLOUDFLARE_TURNSTILE && (process.env.CLOUDFLARE_TURNSTILE.length > 0)) {
-    cloudFlareSiteKey = process.env.CLOUDFLARE_TURNSTILE;
-}
+export const cloudFlareSiteKey = () => {
+    let cfSiteKey ='0x4AAAAAAAVuhgDN4FXyZAFb';
+    if (process.env.CLOUDFLARE_TURNSTILE && (process.env.CLOUDFLARE_TURNSTILE.length > 0)) {
+        cfSiteKey = process.env.CLOUDFLARE_TURNSTILE;
+    }
 
-export cloudFlareSiteKey;
+    return cfSiteKey
+};
