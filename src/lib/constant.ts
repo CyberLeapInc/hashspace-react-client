ba435f4ac01afaf5e9c5d38d9ccf94a3fd5dfe94
anonymous-wan <162077545+anonymous-wan@users.noreply.github.com>
2024-07-18 19:09:46 +0800

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,6 @@
 let cloudFlareSiteKey ='0x4AAAAAAAVuhgDN4FXyZAFb';
 if (process.env.CLOUDFLARE_TURNSTILE && (process.env.CLOUDFLARE_TURNSTILE.length > 0)) {
-    cloudFlareSiteKey = process.env.CLOUDFLARE_TURNSTILE
+    cloudFlareSiteKey = process.env.CLOUDFLARE_TURNSTILE;
 }
 
-export cloudFlareSiteKey 
+export cloudFlareSiteKey;
