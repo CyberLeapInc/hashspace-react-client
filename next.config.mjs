b509920b0e5f28067e3cd2b56c94102e29a04369
anonymous-wan <162077545+anonymous-wan@users.noreply.github.com>
2024-07-18 19:01:36 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    env: {  IS_TEST: process.env.IS_TEST  }
+    env: {  
+        IS_TEST: process.env.IS_TEST,
+        CLOUDFLARE_TURNSTILE: process.env.CLOUDFLARE_TURNSTILE
+    }
 };
 
 export default nextConfig;
