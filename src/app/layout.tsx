c01f563ef1e4a6641ec6b0bd1af25cba04740619
john <164868953+JohnCyberLeap@users.noreply.github.com>
2024-07-30 21:33:43 +0800

    feat: 添加title，description和keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,32 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getLocale, getMessages} from 'next-intl/server';
 import {Providers} from "@/service/providers";
 
+
+let metaDataCn = {
+    title: 'Hash Space - 全球领先的一站式云算力平台',
+    description: 'Hash Space提供全方位的一站式加密数字货币云算力服务，扩展您的加密货币投资潜力。',
+    keywords: ['云算力','比特币挖矿','狗狗币挖矿','云挖矿','云计算','hashspace']
+}
+let metaDataEn = {
+    title: 'Hash Space - Trusted Cloud Mining Platform',
+    description: 'Hash Space offers comprehensive, one-stop cryptocurrency cloud mining services to expand your investment potential in the digital currency realm.',
+    keywords: ['cloud mining bitcoin', 'cloud mining', 'bitcoin mining', 'litecoin mining', 'dogecoin mining', 'crypto cloud mining', 'hashspace']
+}
+let metaDataObj = metaDataEn;
+
+if (typeof window !== 'undefined') {
+    metaDataObj = window.localStorage.getItem('language') === 'en' ? metaDataEn : metaDataCn;
+}
+
 const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 })
 
 export const metadata: Metadata = {
-    title: 'Hash Space',
-    description: 'some Description',
+    title: metaDataObj.title,
+    description: metaDataObj.description,
+    keywords: metaDataObj.keywords
 }
 
 export default async function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
